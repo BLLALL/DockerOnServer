e9105f11afa29d16a087f0b6c080ea7841c94c1c
Belal Amer <bamer8353@gmail.com>
2025-01-01 00:04:26 +0200

    I am stupid

--- src/index.js
+++ src/index.js
@@ -1,41 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const redis = require("redis");
-// const { Client } = require("pg");
-//init app
+// const { Client } = require('pg');
+
+// init app
 const PORT = process.env.PORT || 4000;
 const app = express();
 
-// //connect to redis
-// const REDIS_PORT = 6379;
-// const REDIS_HOST = "redis";
-// const redisClient = redis.createClient({
-//   url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
-// });
-// redisClient.on("error", (err) => console.log("Redis client error", err));
-// redisClient.on("connect", (err) => console.log("connected to redis..."));
-// redisClient.connect();
+// connect to redis
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
 
-//connect to db
-// const DB_USER = "root";
-// const DB_PASSWORD = "example";
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("connected to redis..."));
+redisClient.connect();
+
+// connect db
+// const DB_USER = 'root';
+// const DB_PASSWORD = 'example';
 // const DB_PORT = 5432;
-// const DB_HOST = "postgres";
+// const DB_HOST = 'postgres';
 
 // const URI = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 // const client = new Client({
 //   connectionString: URI,
 // });
-
 // client
 //   .connect()
-//   .then(() => console.log("connected to postgres db..."))
-//   .catch((err) => console.log("failed to connect to postgres db..", err));
-
-// app.get("/", (req, res) => {
-//   redisClient.set("products", "products...");
-//   res.send("<h1> Hello Belal</h1>");
-// });
+//   .then(() => console.log('connected to  postgres db...'))
+//   .catch((err) => console.log('failed to connect to postgres db: ', err));
 
 const DB_USER = "root";
 const DB_PASSWORD = "example";
@@ -46,16 +42,16 @@ const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
 mongoose
   .connect(URI)
   .then(() => console.log("connected to db..."))
-  .catch((err) => console.log("failed to connect to db..", err));
+  .catch((err) => console.log("failed to connect to db: ", err));
 
 app.get("/", (req, res) => {
   redisClient.set("products", "products...");
-  res.send("<h1> Hello Belal</h1>");
+  res.send("<h1> Hello Tresmerge</h1>");
 });
 
 app.get("/data", async (req, res) => {
   const products = await redisClient.get("products");
-  res.send(`<h1> Hello Belal</h1> <h2>${products}</h2>`);
+  res.send(`<h1> Hello Tresmerge!</h1> <h2>${products}</h2>`);
 });
 
 app.listen(PORT, () => console.log(`app is up and running on port: ${PORT}`));
