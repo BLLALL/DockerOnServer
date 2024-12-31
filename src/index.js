9833e449095cf52176a3ac73f19f08577632fe96
Belal Amer <bamer8353@gmail.com>
2025-01-01 01:33:44 +0200

    using dockerhub

--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ mongoose
 
 app.get("/", (req, res) => {
   redisClient.set("products", "products...");
-  res.send("<h1> Hello Tresmerge</h1>");
+  res.send("<h1> Using dockerhub, Elsayed Mtarsham</h1>");
 });
 
 app.get("/data", async (req, res) => {
